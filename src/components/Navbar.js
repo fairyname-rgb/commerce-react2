501fc8251669aea3c058822d7402852cdfc808b9
fairyname-rgb <fairyname17@gmail.com>
2020-06-24 00:53:11 +0300

    App

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import { ButtonContainer } from "./Button";
+import styled from 'styled-components';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                        products
+                    </Link>
+                </li>
+            </ul>
+            <Link to='cart' className='ml-auto'>
+                <ButtonContainer>
+                    <i className="fa fa-cart-plus"></i>
+                    my cart
+                    </ButtonContainer>               
+            </Link>
+        </NavWrapper>
+        )
+    }
+}
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform:capitalize;
+}
+`;
+
